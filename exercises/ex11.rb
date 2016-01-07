099fde6714d7e5c6d3a75dd5d1334239e3c2b422
John McIntosh II <jmci10@hotmail.com>
2016-01-07 02:29:13 +0000

    finish ex11

--- exercises/ex11.rb
+++ exercises/ex11.rb
@@ -1,1 +1,13 @@
+
+# ex11 form
 print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+# study drill form
