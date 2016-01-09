9fd8f9adde70e0d2d8ffcda568cbc28315b782f0
John McIntosh II <jmci10@hotmail.com>
2016-01-09 01:03:40 +0000

    finish ex11 study drill

--- exercises/ex11.rb
+++ exercises/ex11.rb
@@ -1,13 +1,22 @@
 
 # ex11 form
-print "How old are you? "
-age = gets.chomp
-print "How tall are you? "
-height = gets.chomp
-print "How much do you weigh? "
-weight = gets.chomp
+# print "How old are you? "
+# age = gets.chomp
+# print "How tall are you? "
+# height = gets.chomp
+# print "How much do you weigh? "
+# weight = gets.chomp
 
-puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+# puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
 
 
 # study drill form
+print "Please type a noun. "
+noun_one = gets.chomp
+print "Now type an adjective. "
+adjective = gets.chomp
+print "I need another noun from you. "
+noun_two = gets.chomp
+
+puts "You have a(n) #{noun_one}. It is small, unassuming, yet holds #{adjective} power. Your #{noun_one} is the key to unlock the #{noun_two}."
+puts "Are you satisfied with what you have read? If not, try again."
