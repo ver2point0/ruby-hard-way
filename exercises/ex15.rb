a0e90c33fbfea519df115eeeb273618379527860
John McIntosh II <jmci10@hotmail.com>
2016-01-16 07:44:41 +0000

    ex15 complete

--- exercises/ex15.rb
+++ exercises/ex15.rb
@@ -1,13 +1,21 @@
+# use ARGV to get a file name
 filename = ARGV.first
 
+# opening filename and setting it to txt
 txt = open(filename)
 
+# call read on txt
 puts "Here's your file #{filename}:"
 puts txt.read
+txt.close
 
+# asking for the filename again inside the program using STDIN
 puts "Type the filename again: "
 file_again = $stdin.gets.chomp
 
+# opening file_again and setting it to txt_again
 txt_again = open(file_again)
 
+# call read on txt_again
 puts txt_again.read
+txt_again.close
