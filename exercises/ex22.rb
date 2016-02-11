c6a75589e485f936f1f46d490f395ae0437b87d6
John McIntosh II <jmci10@hotmail.com>
2016-02-11 06:03:26 +0000

    ex22 WIP

---
+++ exercises/ex22.rb
@@ -0,0 +1,11 @@
+# make a list of symbols from exercises 1-21
+
+# EXERCISE 1
+# name: puts
+# functionality: prints characters in double qoutes to console
+
+# EXERCISE 2
+# name: # (pound sign)
+# functionality: comments lines out
+
+# EXERCISE 3
