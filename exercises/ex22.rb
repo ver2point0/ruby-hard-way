9d7f83d35bfdd96993963c95b5a5a6d7c52bac07
John McIntosh II <jmci10@hotmail.com>
2016-02-16 11:49:22 +0000

    ex22 session 3

--- exercises/ex22.rb
+++ exercises/ex22.rb
@@ -9,3 +9,10 @@
 # functionality: comments lines out
 
 # EXERCISE 3
+# +(plus): operator to add 2 numbers
+# -(minus): operator to substract 2 numbers
+# *(asterisk): operator to multiply to 2 numbers
+# /(forward slash): operator to divide 2 numbers
+# %(percent): operator used to retrieve remainder of division operation
+# <(less-than): symbol used to check if x is less than y
+# >(greater-than): used to check if x is greater than y
