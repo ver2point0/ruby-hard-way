e95747764ec030eecd80c78b87d766735bc687b9
John McIntosh II <jmci10@hotmail.com>
2016-02-17 12:34:49 +0000

    ex22 session 4

--- exercises/ex22.rb
+++ exercises/ex22.rb
@@ -16,3 +16,13 @@
 # %(percent): operator used to retrieve remainder of division operation
 # <(less-than): symbol used to check if x is less than y
 # >(greater-than): used to check if x is greater than y
+
+# EXERCISE 4
+# my_number = 5: 5 is stored in variable name my_number
+# my_char = 'a': a is stored in variable name my_char
+# my_string = "Hi, how are you?": "Hi, how are you?" is stored in variable name my_string
+# my_boolean = false: false is stored in variable name my_boolean
+# puts "You number is #{my_number}"
+# puts "Your character is #{my_character}"
+# puts "Your string is #{my_string}"
+# puts "#{my_boolean}, you are wrong"
