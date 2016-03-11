aa04532f9e2b6e1e1e5cbf138f1cf2cf794675cd
John McIntosh II <jmci10@hotmail.com>
2016-03-11 12:20:56 +0000

    ex22 session 13

--- exercises/ex22.rb
+++ exercises/ex22.rb
@@ -135,15 +135,21 @@
 # puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
 
 # EXERCISE 12
-print "Give me a number: "
-number = gets.chomp.to_i
+# print "Give me a number: "
+# number = gets.chomp.to_i
 
-bigger = number * 100
-puts "A bigger number is #{bigger}."
+# bigger = number * 100
+# puts "A bigger number is #{bigger}."
 
-print "Give me another number: "
-another = gets.chomp
-number = another.to_i
+# print "Give me another number: "
+# another = gets.chomp
+# number = another.to_i
 
-smaller = number / 100
-puts "A smaller number is #{smaller}."
+# smaller = number / 100
+# puts "A smaller number is #{smaller}."
+
+# EXERCISE 13
+one, two, three = ARGV
+puts "Your 1st variable is: #{one}"
+puts "Your 2nd variable is: #{two}"
+puts "Your 3rd variable is: #{three}"
