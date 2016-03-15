c000d5e810000d4851acad0fb10c3ef233f06651
John McIntosh II <jmci10@hotmail.com>
2016-03-15 02:03:32 +0000

    ex22 session 14

--- exercises/ex22.rb
+++ exercises/ex22.rb
@@ -149,7 +149,28 @@
 # puts "A smaller number is #{smaller}."
 
 # EXERCISE 13
-one, two, three = ARGV
-puts "Your 1st variable is: #{one}"
-puts "Your 2nd variable is: #{two}"
-puts "Your 3rd variable is: #{three}"
+# one, two, three = ARGV
+# puts "Your 1st variable is: #{one}"
+# puts "Your 2nd variable is: #{two}"
+# puts "Your 3rd variable is: #{three}"
+
+# EXERCISE 14
+username = ARGV.first
+prompt = "= "
+
+puts "Hi #{username}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{username}? ", prompt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{username}? ", prompt
+lives = $stdin.gets.chomp
+
+puts "What kind of computer do you have? ", prompt
+computer = $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}. Not sure where that is.
+And you have a #{computer} computer. Nice.
+"""
