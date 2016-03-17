7f1cacf46884972d739747235cd34087dce1b242
John McIntosh II <jmci10@hotmail.com>
2016-03-17 13:22:04 +0000

    ex22 session 17

--- exercises/ex22.rb
+++ exercises/ex22.rb
@@ -225,3 +225,22 @@
 # puts "Close file."
 # t.close
 
+# EXERCISE 17
+from_file, to_file = ARGV
+puts "Copying from #{from_file} to #{to_file}"
+
+in_file = open(from_file)
+in_data = in_file.read
+
+puts "the input file is #{in_data.length} bytes long"
+
+puts "Does the output file exist? #{File.exist?(to_file)}"
+puts "Ready, hit RETURN to continue, CTRL-C to abort."
+$stdin.gets
+
+out_file = open(to_file, "w")
+out_file.write(in_data)
+
+puts "Alright, all done."
+out_file.close
+in_file.close
