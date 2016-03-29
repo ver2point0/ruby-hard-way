67e5cd2a1ce8cfc68aedff60944edb2f9418c0d0
John McIntosh II <jmci10@hotmail.com>
2016-03-29 01:13:19 +0000

    ex22 session 21

--- exercises/ex22.rb
+++ exercises/ex22.rb
@@ -292,44 +292,72 @@
 # cheese_crackers(cheese + 100, crackers + 1000)
 
 # EXERCISE 20
-input_file = ARGV.first
+# input_file = ARGV.first
 
-def print_all(f)
-  puts f.read
-end
+# def print_all(f)
+#   puts f.read
+# end
 
-def rewind(f)
-  f.seek(0)
-end
+# def rewind(f)
+#   f.seek(0)
+# end
 
-def print_a_line(line_count, f)
-  puts "#{line_count}, #{f.gets.chomp}"
-end
+# def print_a_line(line_count, f)
+#   puts "#{line_count}, #{f.gets.chomp}"
+# end
 
-current_file = open(input_file)
+# current_file = open(input_file)
 
-puts "First let's print the whole file:\n"
+# puts "First let's print the whole file:\n"
 
-print_all(current_file)
+# print_all(current_file)
 
-puts "Now let's rewind, kind of like a tape."
+# puts "Now let's rewind, kind of like a tape."
 
-rewind(current_file)
+# rewind(current_file)
 
-puts "Let's print three lines:"
+# puts "Let's print three lines:"
 
-current_line = 1
-print_a_line(current_line, current_file)
+# current_line = 1
+# print_a_line(current_line, current_file)
 
-current_line = current_line + 1
-print_a_line(current_line, current_file)
+# current_line = current_line + 1
+# print_a_line(current_line, current_file)
 
-current_line = current_line + 1
-print_a_line(current_line, current_file)
+# current_line = current_line + 1
+# print_a_line(current_line, current_file)
 
+# EXERCISE 21
+# def add(a, b)
+#   puts "Adding #{a} + #{b}" 
+#   return a + b
+# end
 
+# def subtract(a, b)
+#   puts "Subtracting #{a} - #{b}"
+#   return a - b
+# end 
 
+# def multipy(a, b)
+#   puts "Multiplying #{a} * #{b}"
+#   return a * b
+# end
 
+# def divide(a, b)
+#   puts "Dividing #{a} / #{b}"
+#   return a / b
+# end
 
+# puts "Let's do some math with just functions!"
 
+# age = add(30, 50)
+# height = subtract(78, 4)
+# weight = multipy(90, 2)
+# iq = divide(100, 2)
 
+# puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+# puts "Here's a puzzle."
+# what = add(age, subtract(height, multipy(weight, divide(iq, 2))))
+
+# puts "That becomes: #{what}. Can you do it by hand?"
