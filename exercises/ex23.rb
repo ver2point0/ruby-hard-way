5588467d8682c10aefeaef336d05b397ab5ceb82
John McIntosh II <jmci10@hotmail.com>
2016-04-03 20:46:30 +0000

    continue ex23

--- exercises/ex23.rb
+++ exercises/ex23.rb
@@ -1,3 +1,10 @@
 # Read ruby files on github or another source repo
-# Find a file .rb, start at the top, read through the file, takes on what the code does
+# Find a file .rb, start at the top, read through the file, takes not on what the code does
 # https://github.com/Papillard/ruby: start at wagon0, then work up to wagon4
+
+# github_champ.rb
+
+# prints the string to the console
+puts "I am a github newbie" 
+
+# codestyle_gym.rb
