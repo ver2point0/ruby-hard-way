535009e97265d8f568d3e9703d8ebabbe4ca9f24
John McIntosh II <jmci10@hotmail.com>
2016-04-07 11:23:59 +0000

    more ex23

--- exercises/ex23.rb
+++ exercises/ex23.rb
@@ -17,6 +17,61 @@ class PlayerMoving
     raise "Position must be between 1 and 100" unless GRID_POSITIONS.include? @position
   end
   
-  def move(distanceInMeters)
+  def move(distance_in_meters)
+    @position += distance_in_meters
   end
+  
+  def play
+    if (self.status_player == :play)
+      if rand(1..100) < 50
+        move(rand(1..10))
+      else
+        move(-rand(1..10))
+      end
+    end
+  end
+  
+  def cheat
+    @position = 101
+  end
+  
+  def status_player
+    if (@position > 100)
+      :win
+    elsif (@position < 0) 
+      :loose
+    else
+      :play
+    end
+  end
+  
+  def has_won
+    status_player == :win
+  end
+  
 end
+
+  player = PlayerMoving.new(50)
+  player.play
+  puts player.status_player == :play # true
+  player.cheat
+  puts player.has_won == true
+  
+  begin
+    PlayerMoving.new(110)
+  rescue RuntimeError => e
+    puts e.to_s == "Position must be between 1 and 100" # true
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
