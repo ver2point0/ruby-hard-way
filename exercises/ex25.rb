a3101f31d207b61ef17d9ab907f07b476a9b39cf
John McIntosh II <jmci10@hotmail.com>
2016-04-26 12:13:12 +0000

    continue ex25

--- exercises/ex25.rb
+++ exercises/ex25.rb
@@ -24,4 +24,22 @@ module Ex25
   end
   
   # takes in a full sentence and returns the sorted words
+  def Ex25.sort_sentence(sentence)
+    words = Ex25.break_words(sentence)
+    return Ex25.sort_words(words)
+  end
+  
+  # prints first and last words of the sentence
+  def Ex25.print_first_and_last(sentence)
+    words = Ex25.break_words(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
+  
+  # sorts the words then prints the first and last one
+  def Ex25.print_first_and_last_sorted(sentence)
+    words = Ex25.sort_sentence(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
 end
