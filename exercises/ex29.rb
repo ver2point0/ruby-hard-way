531ef1ae7dd1f0c84ce91d34ecb761afe5f049be
John McIntosh II <jmci10@hotmail.com>
2016-05-20 14:11:07 +0000

    finish ex29

--- exercises/ex29.rb
+++ exercises/ex29.rb
@@ -20,3 +20,14 @@ end
 
 dogs += 5
 
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end 
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+if people == dogs
+  puts "People are dogs."
+end
