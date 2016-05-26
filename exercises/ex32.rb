2bf14b5a86585b073e766fd0c2dda7ccdd041e42
John McIntosh II <jmci10@hotmail.com>
2016-05-26 13:08:35 +0000

    continue ex32

---
+++ exercises/ex32.rb
@@ -0,0 +1,7 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ["apples", "oranges", "pears", "apricots"]
+change = [1, "pennies", 2, "dimes", 3, "quarters"]
+
+for number in the_count
+  puts "This is count #{number}"
+end
