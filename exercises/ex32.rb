5f4d427f0a4601270c25c79bd58af2071c389963
John McIntosh II <jmci10@hotmail.com>
2016-05-27 12:51:49 +0000

    finish ex32

--- exercises/ex32.rb
+++ exercises/ex32.rb
@@ -5,3 +5,22 @@ change = [1, "pennies", 2, "dimes", 3, "quarters"]
 for number in the_count
   puts "This is count #{number}"
 end
+
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+change.each { |i| 
+  puts "I got #{i}" 
+}
+
+elements = []
+
+(0..5).each do |i|
+  puts "adding #{i} to the list."
+  elements.push(i)
+end
+
+elements.each { |i|
+  puts "Element was: #{i}"
+}
