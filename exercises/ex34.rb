6c82b0a61306e9b2b406111e3bdf642a9d3da00a
John McIntosh II <jmci10@hotmail.com>
2016-06-02 14:16:49 +0000

    finish ex34

---
+++ exercises/ex34.rb
@@ -0,0 +1,41 @@
+animals = ["bear", "ruby", "peacock", "kangaroo", "whale", "platypus"]
+
+# animal at 1
+# the animal at 1 is the 2nd animal and is ruby
+# the 2nd animal is at 1 and is ruby
+ruby = animals[1]
+
+# third animal
+# the 3rd animal is at 2 and is a peacock
+# the animal at 2 is the 3rd animal and is a peacock
+peacock = animals[2]
+
+# first animal
+# the 1st animal is at 0 and is a bear
+# the animal at 0 is the 1st animal and is a bear
+bear = animals[0]
+
+# animal at 3
+# the animal at 3 is the 4th animal and is a kangaroo
+# the 4th animal is at 3 and is a kangaroo
+kangaroo = animals[3]
+
+# fifth animal
+# the 5th animal is at 4 and is a whale
+# the animal at 4 is the 5th animal and is a whale
+whale = animals[4]
+
+# animal at 2
+# the animal at 2 is the 3rd animal and is a peacock
+# the 3rd animal is at 2 and is a peacock
+peacock_two = animals[2]
+
+# sixth animal
+# the 6th animal is at 5 and is a whale
+# the animal at 5 is the 6th animal and is a whale
+platypus = animals[5]
+
+# animal at 4
+# the animal at 4 is the 5th animal and is a whale
+# the 5th animal is at 4 and is a whale
+whale_two = animals[4]
