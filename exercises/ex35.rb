5a6db7ace9f02309b2254bca185e3cc031e66493
John McIntosh II <jmci10@hotmail.com>
2016-06-06 12:55:38 +0000

    ex35 continued

--- exercises/ex35.rb
+++ exercises/ex35.rb
@@ -19,7 +19,42 @@ def gold_room
   end
 end
 
+def bear_room
+  puts "There is a bear here."
+  puts "The bear has a bunch of honey."
+  puts "The fat bear is in front of another door."
+  puts "How are you going to move the bear?"
+  bear_moved = false
+  
+  while true
+    print "> "
+    choice = $stdin.gets.chomp
+    if choice == "take honey"
+      dead("The bear looks at you then slaps your face off.")
+    elsif choice == "taunt bear" && !bear_moved
+      puts "The bear has moved from the door. You can go through it now."
+      bear_moved = true
+    elsif choice == "taunt bear" && bear_moved
+      dead("The bear gets pissed off and chews your leg off.")
+    elsif choice == "open door" && bear_moved
+      gold_room
+    else
+      puts "I got no idea what that means."
+    end
+  end
+end
+
+def cthulhu_room
+  
+end
+
 def dead(why)
   puts why, "Good job!"
   exit(0)
 end
+
+def start
+  
+end
+
+start
