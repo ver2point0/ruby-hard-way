dee198378dda1f61ab5cf3c6eb9188dfcdcf3d78
John McIntosh II <jmci10@hotmail.com>
2016-06-13 15:38:31 +0000

    fill start method

--- exercises/ex36.rb
+++ exercises/ex36.rb
@@ -2,6 +2,8 @@
 # 6 rooms, 10 doors, 1 mini boss, 1 normal boss
 
 def room_one
+  puts "You've entered room Room One."
+  exit(0)
 end
 
 def room_two
@@ -19,8 +21,26 @@ end
 def room_six
 end
 
+def dead(why)
+  puts why, "You failed!"
+  exit(0)
+end
 
 def start
+  puts "You don't know how you got here, but you're facing a door."
+  puts "The door has the Roman numeral (I) smeared in blood on it."
+  puts "Do you enter, (Y)es or (N)o?"
+  
+  print "> "
+  choice = $stdin.gets.chomp
+  
+  if (choice === "Y" || choice === "y")
+    room_one
+  elsif (choice === "N" || choice === "n")
+    dead("Aren't you curious what lies beyond door I?")
+  else
+    dead("You stand there until you freeze to death.")
+  end
 end
 
 start
