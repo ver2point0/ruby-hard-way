8ddc18bb021b39eff0226ae15bdf353b97acd332
John McIntosh II <jmci10@hotmail.com>
2016-06-13 13:09:39 +0000

    create code outline

--- exercises/ex36.rb
+++ exercises/ex36.rb
@@ -1,2 +1,26 @@
 # spend a week on my game: due 16 June
 # 6 rooms, 10 doors, 1 mini boss, 1 normal boss
+
+def room_one
+end
+
+def room_two
+end
+
+def room_three
+end
+
+def room_four
+end
+
+def room_five
+end
+
+def room_six
+end
+
+
+def start
+end
+
+start
