22ff21ef0701c9f473266539296d3d1b3bfab447
John McIntosh II <jmci10@hotmail.com>
2016-06-19 21:26:50 +0000

    start keywords ex37

--- exercises/ex37.rb
+++ exercises/ex37.rb
@@ -1,6 +1,21 @@
 # symbol review: due 6/27
 
 # keywords
+# BEGIN: run this block when the script starts
+BEGIN { 
+  puts "hi" 
+}
+
+# END: run this block when the script stops
+END {
+  puts "hi"
+}
+
+# alias: create another name for a function
+alias this_function that_function
+
+# and: logical and, but lower priority than &&
+puts "Hello" and "Goodbye"
 
 # data types
 
