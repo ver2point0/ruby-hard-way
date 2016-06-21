d6964c719580e82717c2cf1fec912b1f042d74a1
John McIntosh II <jmci10@hotmail.com>
2016-06-21 01:26:49 +0000

    continue keywords

--- exercises/ex37.rb
+++ exercises/ex37.rb
@@ -17,6 +17,30 @@ alias this_function that_function
 # and: logical and, but lower priority than &&
 puts "Hello" and "Goodbye"
 
+# begin: start a block, usually for exceptions
+begin
+end
+
+# break: break out a loop right now
+while true
+break
+end
+
+# case: case style conditional like an if
+case X
+  when y
+  else
+end
+
+# class: define a new class
+class Extreme
+end
+
+# def: define a new function
+def some_function()
+end
+
+
 # data types
 
 # string escape sequences
