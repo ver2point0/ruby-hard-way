ddfc10b4d0c413087b7389fae852a15e9a643458
John McIntosh II <jmci10@hotmail.com>
2016-07-06 12:31:07 +0000

    finish ex38

--- exercises/ex38.rb
+++ exercises/ex38.rb
@@ -2,7 +2,7 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 puts "Wait there are not 10 things in that list. Let's fix that."
 
-stuff = ten_things.split(" ")
+stuff = ten_things.split(' ')
 
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
@@ -14,3 +14,13 @@ while stuff.length != 10
   stuff.push(next_one)
   puts "There are #{stuff.length} items now."
 end
+
+puts "There we go: #{stuff}"
+
+puts "Let's do some things with stuff."
+
+puts stuff[1]
+puts stuff[-1]
+puts stuff.pop()
+puts stuff.join(' ')
+puts stuff[3..5].join("#")
