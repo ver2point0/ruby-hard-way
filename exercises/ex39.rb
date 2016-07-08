6487b1d1885663b988ddbaf005c8f7511e75d1ff
John McIntosh II <jmci10@hotmail.com>
2016-07-08 19:51:32 +0000

    continue ex39

--- exercises/ex39.rb
+++ exercises/ex39.rb
@@ -5,5 +5,22 @@ puts things[1] # b
 things[1] = 'z'
 print things # ["a", "b", "c", "d"]
 
-
 # what hashes can do
+stuff = {'name' => 'Zed', 'age' => 39, 'height' => 6 * 12 + 2}
+puts stuff
+puts stuff['name']
+puts stuff['age']
+puts stuff['height']
+stuff['city'] = "San Francisco"
+print stuff['city']
+
+stuff[1] = "Wow"
+stuff[2] = "Neato"
+puts stuff[1]
+puts stuff[2]
+puts stuff
+
+stuff.delete('city')
+stuff.delete(1)
+stuff.delete(2)
+print stuff
