a6f8203c11ac3b059a24a26766844a869bb4ffcc
John McIntosh II <jmci10@hotmail.com>
2016-07-11 12:52:03 +0000

    start hash example

--- exercises/ex39.rb
+++ exercises/ex39.rb
@@ -1,26 +1,44 @@
-# what array list can do
-things = ['a', 'b', 'c', 'd']
-puts things[1] # b
+# # what array list can do
+# things = ['a', 'b', 'c', 'd']
+# puts things[1] # b
 
-things[1] = 'z'
-print things # ["a", "b", "c", "d"]
+# things[1] = 'z'
+# print things # ["a", "b", "c", "d"]
 
-# what hashes can do
-stuff = {'name' => 'Zed', 'age' => 39, 'height' => 6 * 12 + 2}
-puts stuff
-puts stuff['name']
-puts stuff['age']
-puts stuff['height']
-stuff['city'] = "San Francisco"
-print stuff['city']
+# # what hashes can do
+# stuff = {'name' => 'Zed', 'age' => 39, 'height' => 6 * 12 + 2}
+# puts stuff
+# puts stuff['name']
+# puts stuff['age']
+# puts stuff['height']
+# stuff['city'] = "San Francisco"
+# print stuff['city']
 
-stuff[1] = "Wow"
-stuff[2] = "Neato"
-puts stuff[1]
-puts stuff[2]
-puts stuff
+# stuff[1] = "Wow"
+# stuff[2] = "Neato"
+# puts stuff[1]
+# puts stuff[2]
+# puts stuff
 
-stuff.delete('city')
-stuff.delete(1)
-stuff.delete(2)
-print stuff
+# stuff.delete('city')
+# stuff.delete(1)
+# stuff.delete(2)
+# print stuff
+
+# hash example
+# create mapping of state to abbreviation
+states = {
+  "Oregon" => "OR",
+  "Florida" => "FL",
+  "California" => "CA",
+  "New York" => "NY",
+  "Michigan" => "MI"
+}
+
+# create a basic set of states and some cities in them
+cities = {
+  "CA" => "San Francisco",
+  "MI" => "Detroit",
+  "FL" => "Jacksonville"
+}
+
