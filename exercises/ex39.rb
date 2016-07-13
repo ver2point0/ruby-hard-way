afcd43cb28361d753e44f4d3dda10fc947090b59
John McIntosh II <jmci10@hotmail.com>
2016-07-13 13:10:06 +0000

    finish hash example

--- exercises/ex39.rb
+++ exercises/ex39.rb
@@ -61,3 +61,34 @@ puts "-" * 10
 puts "Michigan has: #{cities[states["Michigan"]]}"
 puts "Florida has: #{cities[states["Florida"]]}"
 
+# puts every state abbreviation
+puts '-' * 10
+states.each do |state, abbrev|
+  puts "#{state} is abbreivated #{abbrev}"
+end
+
+# puts every city in state
+puts '-' * 10
+cities.each do |abbrev, city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+# now do both at the same time
+puts '-' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreivated #{abbrev} and has city #{city}"
+end
+
+puts '-' * 10
+# by default ruby says "nil" when something isn't in there
+state = states['Texas']
+
+if !state
+  puts "Sorry, no Texas."
+end
+
+# default values us ||= with nil result
+city = cities['TX']
+city ||= "Does Not Exist"
+puts "The city for the state 'TX' is: #{city}"
