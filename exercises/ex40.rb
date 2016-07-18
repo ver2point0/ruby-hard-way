118be78b1cd30ab95d8cf4a3e695414c8d99931e
John McIntosh II <jmci10@hotmail.com>
2016-07-18 13:21:44 +0000

    finish ex40

--- exercises/ex40.rb
+++ exercises/ex40.rb
@@ -10,3 +10,14 @@ class Song
     }
   end
 end
+
+happy_bday = Song.new(["Happy birthday to you",
+                      "I don't want to get sued",
+                      "So I'll stop right there"])
+                      
+bulls_on_parade = Song.new(["They rally around tha family",
+              "With pockets full of shells"])
+              
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
