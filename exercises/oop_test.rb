0d5858bac4c44ac2a0181fcbd0cf621049ecce49
John McIntosh II <jmci10@hotmail.com>
2016-07-22 20:03:00 +0000

    start oop test

---
+++ exercises/oop_test.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
+
+PHRASES = {
+  
+}
+
+PHRASE_FIRST = ARGV[0] == "english"
+
+open(WORD_URL) {
+  
+}
+
+def craft_names()
+  
+end
+
+def craft_params()
+  
+end
+
+def convert()
+  
+end
+
+loop do
+  
+end
