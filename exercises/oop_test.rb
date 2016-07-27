1f850d5c0b3ea2ae1e480eb94467d9eafb44705b
John McIntosh II <jmci10@hotmail.com>
2016-07-27 13:30:06 +0000

    more oop test

--- exercises/oop_test.rb
+++ exercises/oop_test.rb
@@ -6,20 +6,32 @@ WORDS = []
 PHRASES = {
   "class ### < ###\nend" => "Make a class named ### this is-a ###.",
   "class ###\n\tdef initialize(@@@)\n\ntend\nend" => "class ### has-a initialize that takes @@@ parameters.",
+  "class ###\n\tdef ***(@@@)\n\tend\nend" => "class ### has-a function name *** that takes @@@ parameters.",
+  "*** = ###.new()" => "Set *** to an instance of class ###.",
+  "***.***(@@@)" => "From *** get the *** function, and call it with parameters @@@.",
+  "***.*** = '***'" => "From *** get the *** attribute and set it to '***'."
 }
 
 PHRASE_FIRST = ARGV[0] == "english"
 
-open(WORD_URL) {
-  
+open(WORD_URL) { |f|
+  f.each_line { |word| WORDS.push(word.chomp) }
 }
 
-def craft_names()
+def craft_names(rand_words, snippet, pattern, caps=false)
+  names = snippet.scan(pattern).map do
+    word = rand_words.pop()
+    caps ? word.capitalize : word 
+  end
   
+  return names * 2
 end
 
-def craft_params()
+def craft_params(rand_words, snippet, pattern)
+  names = ().map do
+  end
   
+  return names * 2
 end
 
 def convert()
