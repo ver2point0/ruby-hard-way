f116028bba86b4c5d43e70f2d50321fc7a57d4f3
John McIntosh II <jmci10@hotmail.com>
2016-08-10 13:15:02 +0000

    continue ex42

--- exercises/ex42.rb
+++ exercises/ex42.rb
@@ -2,3 +2,34 @@
 class Animal
 end
 
+# Dog is-a Animal
+class Dog < Animal
+  def initialize(name)
+    # Dog has-a name
+    @name = name
+  end
+end
+
+# Cat is-a Animal
+class Cat < Animal
+  def initialize(name)
+    # Cat has-a name
+    @name = name
+  end
+end
+
+# Person is-a object
+class Person
+  
+  def initialize(name)
+    # Person has-a name
+    @name = name
+    
+    # Person has-a pet of some kind
+    @pet = nil
+  end
+  
+  attr_accessor :pet
+end
+
+
