1e25e7cce1783048f24f2eb028f5744e62b738f4
John McIntosh II <jmci10@hotmail.com>
2016-08-18 13:14:26 +0000

    continue ex43

--- exercises/ex43.rb
+++ exercises/ex43.rb
@@ -18,21 +18,43 @@
 class Scene
   
   def enter()
+    puts "This scene is not yet configured. Subclass it and implement enter()."
+    exit(1)
   end
 end
 
 class Engine
   
   def initialize(scene_map)
+    @scene_map = scene_map
   end
   
   def play()
+    current_scene = @scene_map.opening_scene()
+    last_scene = @scene_map.next_scene("finished")
+    
+    while current_scene != last_scene
+      next_scene_name = current_scene.enter()
+      current_scene = @scene_map.next_scene(next_scene_name)
+    end
+    
+    # make sure to print out the last scene
+    current_scene.enter()
   end
 end
 
 class Death < Scene
   
+  @@quips = [
+    "You died. You kinda suck at this.",
+    "Your mom would be proud...if she were smarter.",
+    "Such a luser.",
+    "I have a small puppy that's better at this."
+    ]
+  
   def enter()
+    puts @@quips[rand(0..(@@quips.length - 1))]
+    exit(1)
   end
 end
 
