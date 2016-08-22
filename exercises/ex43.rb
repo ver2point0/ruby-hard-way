1c39dba5ee69fb05d59086095955f9e00199550c
John McIntosh II <jmci10@hotmail.com>
2016-08-22 13:17:11 +0000

    continue writing central corridor

--- exercises/ex43.rb
+++ exercises/ex43.rb
@@ -67,7 +67,10 @@ class CentralCorridor < Scene
     puts "put it in the bridge, and blow the ship up after getting into an"
     puts "escape pod."
     puts "\n"
-    
+    puts "You're running down the central corridor to the Weapons Armory when"
+    puts "a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume"
+    puts "flowing around his hate filled body. He's blocking the door to the"
+    puts "Armory and about to pull a weapon to blast you."
     
     print"> "
     
