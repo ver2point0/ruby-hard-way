187f4d94ca84499749a674b37bf32c46e0fa2505
John McIntosh II <jmci10@hotmail.com>
2016-08-23 19:34:22 +0000

    finish writing central corridor

--- exercises/ex43.rb
+++ exercises/ex43.rb
@@ -77,18 +77,30 @@ class CentralCorridor < Scene
     action = $stdin.gets.chomp
     
     if (action == "shoot!")
-      puts ""
-      
+      puts "Quick on the draw you yank out your blaster and fire it at the Gothon."
+      puts "His clown costume is flowing and moving around his body, which throws"
+      puts "off your aim. Your laser hits his costume but misses him entirely. This"
+      puts "completely ruins his brand new costume his mother bought him, which"
+      puts "makes him fly into an insane rage and blast you repeatedly in the face until"
+      puts "you are dead. Then he eats you."
       return 'death'
       
     elsif (action == "dodge!")
-      puts ""
-      
+      puts "Like a world class boxer you dodge, weave, slip and slide right"
+      puts "as the Gothon's blaster cranks a laser past your head."
+      puts "In the middle of your artful dodge your foot slips and you"
+      puts "bang your head on the metal wall and pass out."
+      puts "You wake up shortly after only to die as the Gothon stomps on"
+      puts "your head and eats you."
       return 'death'
       
     elsif (action == "tell a joke")
-      puts ""
-      
+      puts "Lucky for you they made you learn Gothon insults in the academy."
+      puts "You tell the one Gothon joke you know:"
+      puts "Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhf."
+      puts "The Gothon stops, tries not to laugh, then busts out laughing and can't move."
+      puts "While he's laughing you run up and shoot him square in the head"
+      puts "putting him down, then jump through the Weapon Armory door."
       return 'laser_weapon_armory'
     
     else
