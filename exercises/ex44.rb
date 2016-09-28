806b26e408a02f4f71936883320ab5ec7941c3d5
John McIntosh II <jmci10@hotmail.com>
2016-09-28 14:08:06 +0000

    use super() with initialize

--- exercises/ex44.rb
+++ exercises/ex44.rb
@@ -1,89 +1,99 @@
-# Implicit inheritance
+# # Implicit inheritance
 
-class Parent
+# class Parent
   
-  def implicit()
-    puts "PARENT implicit()"
-  end
-end
+#   def implicit()
+#     puts "PARENT implicit()"
+#   end
+# end
 
-class Child < Parent
-end
+# class Child < Parent
+# end
 
-dad = Parent.new()
-son = Child.new()
+# dad = Parent.new()
+# son = Child.new()
 
-dad.implicit()
-son.implicit()
+# dad.implicit()
+# son.implicit()
 
-# override explicitly
+# # override explicitly
 
-class ParentTwo
+# class ParentTwo
   
-  def override()
-    puts "PARENT override()"
-  end
-end
+#   def override()
+#     puts "PARENT override()"
+#   end
+# end
 
-class ChildTwo < ParentTwo
+# class ChildTwo < ParentTwo
   
-  def override()
-    puts "CHILD override()"
-  end
-end
+#   def override()
+#     puts "CHILD override()"
+#   end
+# end
 
-dad2 = ParentTwo.new()
-son2 = ChildTwo.new()
+# dad2 = ParentTwo.new()
+# son2 = ChildTwo.new()
 
-dad2.override()
-son2.override()
+# dad2.override()
+# son2.override()
 
-# after before or after
-class ParentThree
+# # after before or after
+# class ParentThree
   
-  def altered()
-    puts "PARENT altered()"
-  end
-end
+#   def altered()
+#     puts "PARENT altered()"
+#   end
+# end
 
-class ChildThree < ParentThree
+# class ChildThree < ParentThree
   
-  def altered() 
-    puts "CHILD, BEFORE PARENT altered()"
-    super()
-    puts "CHILD, AFTER PARENT altered()"
-  end
-end
+#   def altered() 
+#     puts "CHILD, BEFORE PARENT altered()"
+#     super()
+#     puts "CHILD, AFTER PARENT altered()"
+#   end
+# end
 
-dad3 = ParentThree.new()
-son3 = ChildThree.new()
+# dad3 = ParentThree.new()
+# son3 = ChildThree.new()
 
-dad3.altered()
-son3.altered()
+# dad3.altered()
+# son3.altered()
 
 # all three combined
 class P4
   
+  # declare override() method: puts "parent override()" to the console
   def override()
     puts "parent override()"
   end
   
+  # declare implicit() method: puts "parent implicit()" to the console
   def implicit()
     puts "parent implicit()"
   end
   
+  # declare altered() method: puts "parent override()" to the console
   def altered()
-    puts "Parent altered()"
+    puts "parent altered()"
   end
 end
 
-
+# child class C4 inherits from parent class P4
 class C4 < P4
 
+  def initialize(stuff)
+    @stuff = stuff
+    super()
+  end
+  
+  # declare override() method: puts "child override()" to the console
   def override()
     puts "child override()"
   end
   
+  # declare altered() method: puts "child, before parent altered()", "parent altered()", "child, after parent altered()" to the console
   def altered()
     puts "child, before parent altered()"
     super()
@@ -92,7 +102,7 @@ class C4 < P4
 end
 
 p4 = P4.new()
-c4 = C4.new()
+c4 = C4.new("child initialize")
 
 p4.implicit()
 c4.implicit()
